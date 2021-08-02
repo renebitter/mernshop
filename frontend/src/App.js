4c6b1f6b15aabefb0bdeba150945e173552a3421
Rene <renebitter@gmail.com>
2021-08-02 12:29:37 -0300

    Create first components and favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header />
+      <main>
+        <h1>Welcome to ProShop</h1>
+      </main>
+      <Footer />
     </>
   );
 };
