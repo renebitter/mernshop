36d6be8d0867e18df3e36291a8ad6fc7243a4c23
Rene <renebitter@gmail.com>
2021-08-06 16:57:01 -0300

    DB Connection

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
