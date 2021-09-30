ae877c8e56778e5291ef61ad358b39f23bc5acad
Rene <renebitter@gmail.com>
2021-09-30 16:00:25 -0300

    Production server

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import path from 'path';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -17,25 +17,34 @@ connectDB();
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  // app.use(morgan('dev'));
+
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
 }
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
+
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+}
 
 app.use(notFound);
 app.use(errorHandler);
