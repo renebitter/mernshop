04b0ecc8c88ec2b9cc5da1165c6138cbd4ec49f2
Rene Bitter <renebitter@gmail.com>
2023-09-20 16:20:00 -0300

    Refactor styling

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -21,7 +21,7 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant='danger'>{error}</Message>
   ) : (
-    <Carousel pause='hover'>
+    <Carousel pause='hover' slide={false} data-bs-theme='dark'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -29,7 +29,7 @@ const HomeScreen = ({ match }) => {
       <Meta />
       {!keyword ? (
         <>
-          <Hero />
+          {/* <Hero /> */}
           <ProductCarousel />
         </>
       ) : (
